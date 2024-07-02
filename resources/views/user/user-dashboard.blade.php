042ec40f5c17ca723910764c1f0d7598ea5fc91e
RahmanIslamIen <rahmanislamien@.com>
2024-07-03 02:04:34 +0700

    perbaikan ui user karyawan

--- resources/views/user/user-dashboard.blade.php
+++ resources/views/user/user-dashboard.blade.php
@@ -168,7 +168,7 @@
                 "responsive": true,
                 "lengthChange": false,
                 "autoWidth": false,
-                "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+                // "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
             }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
             $('#example2').DataTable({
                 "paging": true,
