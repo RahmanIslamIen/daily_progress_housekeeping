364cc858e5653354a9bab9172af54bf0c63883b2
RahmanIslamIen <rahmanislamien@gmail.com>
2024-07-13 23:07:20 +0700

    update ui kerusakan toilet

--- resources/views/admin/laporan_kerusakan/data-kerusakan-toilet/semua-kerusakan-toilet.blade.php
+++ resources/views/admin/laporan_kerusakan/data-kerusakan-toilet/semua-kerusakan-toilet.blade.php
@@ -35,36 +35,56 @@
             </div>
             <!-- /.card-header -->
             <div class="card-body">
-                <p>kd karyawan : {{ $semuaData->kd_karyawan }}</p>
-                <p>kd toilet :{{ $semuaData->kd_toilet }}</p>
-                <p>tanggal pembuatan : {{ $semuaData->tanggal_pembuatan }}</p>
-                <p>tanggal kejadian : {{ $semuaData->tanggal_kejadian }}</p>
-                <p>nama kerusakan : {{ $semuaData->nama_kerusakan }}</p>
-                <p>lokasi kerusakan :{{ $semuaData->lokasi_kerusakan }}</p>
-                <p>kronologi kerusakan : {{ $semuaData->kronologi_kerusakan }}</p>
-                <p>tindakan : {{ $semuaData->tindakan }}</p>
-                <p>lampiran foto :
-                    <img src="{{ asset('storage/' . $semuaData->lampiran_foto) }}" alt="Lampiran Foto"
-                        style="position: absolute; object-fit: contain; border-radius: 20px; 
+
+                <div class="row">
+                    <div class="col">
+                        <p>kd karyawan : {{ $semuaData->kd_karyawan }}</p>
+                        <p>kd toilet :{{ $semuaData->kd_toilet }}</p>
+                    </div>
+                    <div class="col">
+                        <p>tanggal pembuatan : {{ $semuaData->tanggal_pembuatan }}</p>
+                        <p>tanggal kejadian : {{ $semuaData->tanggal_kejadian }}</p>
+                    </div>
+                    <div class="col">
+                        <p>lampiran foto :</p>
+                        <img src="{{ asset('storage/' . $semuaData->lampiran_foto) }}" alt="Lampiran Foto"
+                            style="position: absolute; object-fit: contain; border-radius: 20px; 
                                     max-width: 250px; max-height: 250px; translate: z-index: 10;" />
-                </p>
-                <p>yang melaporkan :{{ $semuaData->yang_melaporkan }}</p>
-                <p>yang mengetahui : {{ $semuaData->yang_mengetahui }}</p>
-                <p>catatan perbaiakan : {{ $semuaData->catatan_perbaikan }}</p>
-                <p>yang mengerjakan : {{ $semuaData->yang_mengerjakan }}</p>
-                <div style="display: inline-block; float: right;">
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col">
+                        <p>nama kerusakan : {{ $semuaData->nama_kerusakan }}</p>
+                        <p>lokasi kerusakan :{{ $semuaData->lokasi_kerusakan }}</p>
+                        <p>kronologi kerusakan : {{ $semuaData->kronologi_kerusakan }}</p>
+                        <p>tindakan : {{ $semuaData->tindakan }}</p>
+                    </div>
+                    <div class="col">
+                        <p>yang melaporkan :{{ $semuaData->yang_melaporkan }}</p>
+                        <p>yang mengetahui : {{ $semuaData->yang_mengetahui }}</p>
+                        <p>catatan perbaiakan : {{ $semuaData->catatan_perbaikan }}</p>
+                        <p>yang mengerjakan : {{ $semuaData->yang_mengerjakan }}</p>
+                    </div>
+                    <div class="col">
+                    </div>
+                </div>
+
+
+                <div style="display: inline;">
                     <table>
                         <tr>
-                            <td><a href="/print-pdf-keruskan-toilet/{{ $semuaData->id }}" class="btn btn-warning"
-                                    target="_blank">Print PDF</a>
+                            <td><a href="/print-pdf-keruskan-toilet/{{ $semuaData->id }}"
+                                    class="btn btn-sm btn-warning m-1" target="_blank">Print PDF</a>
                             </td>
-                            <td><a href="ubah-kerusakan-toilet/{{ $semuaData->id }}" class="btn btn-s btn-info">edit</a>
+                            <td><a href="ubah-kerusakan-toilet/{{ $semuaData->id }}"
+                                    class="btn btn-sm btn-info m-1">edit</a>
                             </td>
                             <td>
                                 <form method="POST" action="/hapus-kerusakan-toilet/{{ $semuaData->id }}">
                                     @csrf
                                     @method('DELETE')
-                                    <button type="submit" class="btn btn-s btn-danger"
+                                    <button type="submit" class="btn btn-sm btn-danger m-1"
                                         onclick="return confirm('Apakah Anda yakin ingin menghapus data?')">Delete</button>
                                 </form>
                             </td>
